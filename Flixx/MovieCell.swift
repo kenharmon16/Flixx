3e0757ff44384761e273376ef012ae9fd21a8531
kenharmon16 <kenharmon16@gmail.com>
2020-03-03 19:47:39 -0600

    initial commit

---
+++ Flixx/MovieCell.swift
@@ -0,0 +1,30 @@
+//
+//  MovieCell.swift
+//  Flixx
+//
+//  Created by ken harmon on 3/3/20.
+//  Copyright © 2020 ken harmon. All rights reserved.
+//
+
+import UIKit
+
+class MovieCell: UITableViewCell {
+
+    @IBOutlet weak var titleLabel: UILabel!
+    
+    @IBOutlet weak var synopsisLabel: UILabel!
+    
+    @IBOutlet weak var posterView: UIImageView!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
